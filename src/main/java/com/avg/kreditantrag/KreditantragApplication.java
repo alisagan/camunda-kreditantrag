dc2ed9b731bcfb2fb53be8fed8e7a6b877b92d03
Alisa Ganter <gaal1019@h-ka.de>
2025-05-31 20:13:04 +0200

    application.yaml angepasst

---
+++ src/main/java/com/avg/kreditantrag/KreditantragApplication.java
@@ -0,0 +1,13 @@
+package com.avg.kreditantrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreditantragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreditantragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avg/kreditantrag/KreditantragApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avg.kreditantrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreditantragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
